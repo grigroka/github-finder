408936512f6b5446e74407a859793016b392f860
Rokas Grigaravicius <grigroka@gmail.com>
2018-11-21 11:33:04 +0200

    http call

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 'use strict';
+// Init Github
+const github = new GitHub();
 // Search input
 const searchUser = document.querySelector('#searchUser');
 
@@ -7,7 +9,18 @@ searchUser.addEventListener('keyup', e => {
   // Get input text
   const userText = e.target.value;
   if (userText !== '') {
-    console.log(userText);
+    // Make http call
+    github
+      .getUser(userText)
+      .then(data => {
+        if (data.profile.message === 'Not Found') {
+          // Show alert
+        } else {
+          // Show profile
+        }
+      })
+      .catch(err => {});
   } else {
+    // Clear profile
   }
 });
