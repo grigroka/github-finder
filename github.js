8ade27afd5b70921f83ef078dd21e3dcb8c534fd
Rokas Grigaravicius <grigroka@gmail.com>
2018-11-21 12:17:49 +0200

    get repos http request

--- github.js
+++ github.js
@@ -3,6 +3,8 @@ class GitHub {
   constructor() {
     this.client_id = '91c20f9143350c5a15cb';
     this.client_secret = '7a74a06bf05415e6b8712ed665513f818a877e7a';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
@@ -11,9 +13,19 @@ class GitHub {
         this.client_id
       }&client_secret=${this.client_secret}`
     );
+    const reposResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${
+        this.repos_count
+      }&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${
+        this.client_secret
+      }`
+    );
     const profile = await profileResponse.json();
+    const repos = await reposResponse.json();
+
     return {
-      profile
+      profile,
+      repos
     };
   }
 }
